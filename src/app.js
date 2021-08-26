5acfb74d70048bff69a503f0abfbe67f6e3e8171
Suyog <suyogschavan03@gmail.com>
2021-08-26 10:22:04 +0530

    Patch & Delete Request added

--- src/app.js
+++ src/app.js
@@ -46,20 +46,48 @@ app.get("/clients/:id", async (req, res) => {
          console.log(clientData);
 
 if(!clientData){
-    return res.status(404).send();
-}
-else{
-    res.send(clientData);
-}
+        return res.status(404).send();
+    }
+    else{
+        res.send(clientData);
+    }
 
     }catch(e){
-res.send(e);
+        res.send(e);
+    }
+})
+
+// Update the students by ID
+
+app.patch("/clients/:id", async (req, res) => {
+    try{
+        const _id = req.params.id;
+        const updateClient = await Client.findByIdAndUpdate(_id, req.body );
+        res.send(updateClient);
+    }catch(e){
+        /* const _id = params.id;
+        const updateClient = await Client.findByIdAndUpdate(_id, req.body ); */
+        res.status(404).send(e);
     }
 })
 
 
+// Delete the client by id
+app.delete("/clients/:id", async (req, res) => {
+    try{
+        const id = req.params.id;
+        const deleteClient = await Client.findByIdAndDelete(id);
+        if(!id){
+            return res.status(404).send("id not found");
+        }
+        res.send(deleteClient);
+    }catch(e){
+        res.status(500).send(e);
+
+    }
+})
 
 
-app.listen(port, () => {
+app.listen(port, () => { 
     console.log(`connection is setup at ${port}`);
 })
